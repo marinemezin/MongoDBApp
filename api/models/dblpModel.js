193d24922dd374f26611d71c61e564b8cc4a4e5c
Unknown <diidiie159@gmail.com>
2018-03-21 14:49:09 +0100

    b

--- api/models/dblpModel.js
+++ api/models/dblpModel.js
@@ -14,7 +14,7 @@ var PublicationSchema = new Schema({
 		required : 'Type of publication'
 	},
 	year : {
-		type : int32,
+		type : double,
 		required : 'Year of publication'
 	},
 	title : {
@@ -27,11 +27,11 @@ var PublicationSchema = new Schema({
 	},
 	pages : {
 		start : {
-		type : int32,
+		type : double,
 		required : 'Page of start of publication'
 		},
 		end : {
-		type : int32,
+		type : double,
 		required : 'Page of end of publication'
 		}
 	},
