f0c56b4e291261dc03417566919fec3e30ac5a48
marinemarine <mezin.marine@gmail.com>
2018-03-21 14:06:32 +0100

    app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+//Modules
+var express = require('express'),
+    app = express(),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
+
+//Models
+var DBLP = require('./api/models/dblpModel');
+
+/*global.config = require('./modules/config');
+global.db = require('./modules/db');*/
+
+//mongoose.Promise = global.Promise;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/dblpRoutes');
+routes(app);
+
+var port = process.env.PORT || config.PORT;
+
+app.listen(port, () => {
+    console.log('RESTful API server started on: ' + port);
+});
