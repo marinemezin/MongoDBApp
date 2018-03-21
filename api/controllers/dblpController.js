27c820de6d6adabd437b51ea580cd2e46a361d4c
marinemarine <mezin.marine@gmail.com>
2018-03-21 15:33:53 +0100

    changes

--- api/controllers/dblpController.js
+++ api/controllers/dblpController.js
@@ -7,21 +7,41 @@ var DBLP = db.model('publis');
 
 //Research
 exports.launchQuery1 = function (req, res) {
-    DBLP.getCollection('publis').find({"authors":"Matthieu Cord"}, {"title":1, "booktitle":1, "year":1})
+    DBLP.find({ "authors": "Matthieu Cord" }, { "title": 1, "booktitle": 1, "year": 1 }).then(function (dblps) {
+        res.status(200).json(dblps);
+    }).catch(function (err) {
+        errorHandler.error(res, err.message, "Failed to get publications");
+    });
 };
 
 exports.launchQuery2 = function (req, res) {
-	DBLP.getCollection('publis').find ({"booktitle":"SmartKom"},{"title":1})
+    DBLP.find({ "booktitle": "SmartKom" }, { "title": 1 }).then(function (dblps) {
+        res.status(200).json(dblps);
+    }).catch(function (err) {
+        errorHandler.error(res, err.message, "Failed to get publications");
+    });
 };
 
 exports.launchQuery3 = function (req, res) {
-	DBLP.getCollection('publis').find({"type":"Article", "booktitle":"Machine Learning Techniques for Multimedia"},{"title":1, "authors":1})
+    DBLP.find({ "type": "Article", "booktitle": "Machine Learning Techniques for Multimedia" }, { "title": 1, "authors": 1 }).then(function (dblps) {
+        res.status(200).json(dblps);
+    }).catch(function (err) {
+        errorHandler.error(res, err.message, "Failed to get publications");
+    });
 };
 
 exports.launchQuery4 = function (req, res) {
-	DBLP.getCollection('publis').aggregate([$match : {"authors" : "Massimo Zacanaro"}},{$project : {"title":1, "year":1}},{$sort : {"year":1}}]);
+    DBLP.aggregate([{ "$match": { "authors": "Massimo Zacanaro" } }, { $project: { "title": 1, "year": 1 } }, { $sort: { "year": 1 } }]).then(function (dblps) {
+        res.status(200).json(dblps);
+    }).catch(function (err) {
+        errorHandler.error(res, err.message, "Failed to get publications");
+    });
 };
 
 exports.launchQuery5 = function (req, res) {
-	DBLP.getCollection('publis').aggregate([{"$unwind":"$authors"},{$group : {"_id":"$authors", "mean_year_pub" : {$avg : "$year"}}},{$sort : {"mean_year_pub":1}}])
+    DBLP.aggregate([{ "$unwind": "$authors" }, { $group: { "_id": "$authors", "mean_year_pub": { $avg: "$year" } } }, { $sort: { "mean_year_pub": 1 } }]).then(function (dblps) {
+        res.status(200).json(dblps);
+    }).catch(function (err) {
+        errorHandler.error(res, err.message, "Failed to get publications");
+    });
 };
