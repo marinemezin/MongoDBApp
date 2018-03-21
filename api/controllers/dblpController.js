3a18eeb53d722a7efd6461c2a463a2e7548af163
marinemarine <mezin.marine@gmail.com>
2018-03-21 14:52:40 +0100

    modif

--- api/controllers/dblpController.js
+++ api/controllers/dblpController.js
@@ -1,5 +1,9 @@
-//Importation
-
+//Modules
+var mongoose = require('mongoose');
+//Utils
+var errorHandler = require('../handlers/dblpHandler');
+//Model
+var DBLP = db.model('Publication');
 
 //Research
 exports.launchQuery1 = function (req, res) {
