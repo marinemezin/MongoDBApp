b56f862d49532af333d9565b9eca9177cad9b8f1
Unknown <diidiie159@gmail.com>
2018-03-21 14:42:40 +0100

    model ok

--- api/models/dblpModel.js
+++ api/models/dblpModel.js
@@ -4,7 +4,67 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var A  
+var PublicationSchema = new Schema({
+	_id : {
+		type : String,
+		required : 'Id'
+	},
+	type : {
+		type : String,
+		required : 'Type of publication'
+	},
+	year : {
+		type : float,
+		required : 'Year of publication'
+	},
+	title : {
+		type : String,
+		required : 'Title of publication'
+	},
+	authors : {
+		type : [String],
+		required : 'Author(s) of publication'
+	},
+	pages : {
+		start : {
+		type : int,
+		required : 'Page of start of publication'
+		},
+		end : {
+		type : int,
+		required : 'Page of end of publication'
+		}
+	},
+	booktitle : {
+		type : String,
+		required : 'Title of booktitle of publication'
+	},
+	journal : {
+		series : {
+			type : String,
+			required : 'Series of journal of publication'
+		},
+		editor : {
+			type : String,
+			required : 'Editor of journal of publication'
+		},
+		volume : {
+			type : String,
+			required : 'Volume of journal of publication'
+		},
+		isbn : {
+			type : [String],
+			required : 'ISBN(s) of journal of publication'
+		}		
+	},
+	url : {
+		type : String,
+		required : 'Link of publication'
+	},
+	cities : {
+		type : [String],
+		required : 'City/Cities of publication'
+	}
+}, { versionKey: false });
 
-
-FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+module.exports = mongoose.model('Publication', PublicationSchema);
