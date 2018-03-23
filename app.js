e0c8b86440c694fa67842c81ad6e77ed1adb00f5
Jerem <jeremy.miara@devinci.fr>
2018-03-23 09:34:17 +0100

    Test affichage html

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 'use strict';
 
 //Modules
+var fs = require('fs');
+var sys = require('sys'); 
 var express = require('express'),
     app = express(),
     //mongoose = require('mongoose'),
     bodyParser = require('body-parser');
+
+    app.use(express.logger());
+    app.set("view options", {layout: false});
+    app.use(express.static(__dirname + '/views'));
+
 
 //Models
 //var DBLP = require('./api/models/dblpModel');
@@ -25,4 +32,8 @@ var port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log('RESTful API server started on: ' + port);
+});
+
+app.get('/', function(req, res){
+    res.render('/views/index.html');
 });
