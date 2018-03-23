42f1fb2f0ae1a8fea693fcc0065fbc43cb852f59
marinemarine <mezin.marine@gmail.com>
2018-03-23 10:12:36 +0100

    css

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var express = require('express'),
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/css', express.static('api/views', { root: __dirname }));
+app.use(express.static("./api/views"));
 
 app.get('/', function (req, res) {
     res.sendFile('api/views/index.html', { root: __dirname });
