82c0a636e6f4e4fadf564e3fe5ed9717ff2cfbd2
marinemarine <mezin.marine@gmail.com>
2018-03-23 09:37:03 +0100

    html ok

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
 'use strict';
 
 //Modules
-var fs = require('fs');
-var sys = require('sys'); 
 var express = require('express'),
     app = express(),
-    //mongoose = require('mongoose'),
     bodyParser = require('body-parser');
-
-    app.use(express.logger());
-    app.set("view options", {layout: false});
-    app.use(express.static(__dirname + '/views'));
-
-
-//Models
-//var DBLP = require('./api/models/dblpModel');
-
-/*global.config = require('./modules/config');
-global.db = require('./modules/db');
-
-mongoose.Promise = global.Promise;*/
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get('/', function (req, res) {
+    res.sendFile('api/views/index.html', { root: __dirname });
+});
 
 var routes = require('./api/routes/dblpRoutes');
 routes(app);
